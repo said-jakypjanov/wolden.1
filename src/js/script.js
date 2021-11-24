b718011b9b8aa4f0f99f20ebf14cbbb64a71b86f
said-jakypjanov <saidjakypjanov82@gmail.com>
2021-11-24 12:25:25 +0600

    first commit

---
+++ src/js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $(".carousel").owlCarousel({
+        items:4,
+        autoplay:true,
+        autoplayHoverPause:true,
+        autoplayTimeout:1500,
+        margin: 36,
+        center: true,
+        loop: true,
+    });
+});
